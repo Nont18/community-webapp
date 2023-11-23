35d1a4e227cba97c2a0bfc8709bc9ee44bebe2e8
Nont Arayarungsarit <st124335@ait.asia>
2023-11-23 06:24:49 +0000

    Update 20231112142023_add_link_id_to_phone_lists.rb

--- db/migrate/20231112142023_add_link_id_to_phone_lists.rb
+++ db/migrate/20231112142023_add_link_id_to_phone_lists.rb
@@ -1,6 +1,5 @@
 class AddLinkIdToPhoneLists < ActiveRecord::Migration[7.0]
   def change
-    # add_column :phone_lists, :linkid, :integer
-    add_column :phone_lists, :integer
+    add_column :phone_lists, :linkid, :integer
   end
 end
